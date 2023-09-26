ded385bd741b54cf992c37b5254c2a49af0e8b52
cr4yth <abixshek@gmail.com>
2023-09-26 22:45:29 +0530

    responsive quiz

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import ques from "../jsons/ques.json";
+import styles from "./quiz.module.css";
 
 const Quiz = () => {
   const que = ques.questions;
@@ -19,28 +20,25 @@ const Quiz = () => {
     }
   };
   return (
-    <div className="king flex flex-col justify-center items-center w-[81vw] h-[80vh] ml-auto mr-auto">
-      <div className="flex shadow-[5px_5px_rgba(218,58,0,0.5),5px_5px_rgba(218,58,0,0.4),10px_10px_rgba(218,58,0,0.3),15px_15px_rgba(218,58,0,0.2),20px_20px_rgba(218,58,0,0.1),25px_25px_rgba(218,58,0,0.05)]">
+    <div className={styles.king}>
+      <div className={styles.subking}>
         <div>
-          <img
-            className="quiz-img w-[35vw] h-[80vh] object-cover"
-            src="./qui.webp"
-          ></img>
+          <img className={styles.quizimg} src="./qui.webp"></img>
         </div>
-        <div className="flex flex-col bg-white pt-[2rem] pb-[2rem]">
-          <div className="text-[1.35rem]">
-            Question{" "}
-            <span className="text-[1.35rem] text-[#da3a00]">{qno + 1}</span>
-            <span className="text-[1.05rem]">/17</span>
+        <div className={styles.form}>
+          <div className={styles.ques}>
+            <span>Question</span>
+            <span>{qno + 1}</span>
+            <span>/17</span>
           </div>
-          <div className="question w-[40vw] text-[1.8rem] text-balance mb-[1rem] mt-[0.5rem]">
-            {que[qno].question}
+          <div className={styles.question}>
+            <span className={styles.q}>{que[qno].question}</span>
           </div>
-          <div className="answer flex flex-col mt-[1rem]">
+          <div className={styles.answer}>
             {answers?.map((dat) => {
               return (
-                <div className="border-solid border-[1px] border-[#da3a00] p-[6px] mb-[20px] w-[18.6rem] transition-all duration-200 hover:scale-[1.08] shadow-[3.5px_3.5px_rgba(218,58,0,0.85)]">
-                  <label className="text-[1.4rem] flex" for={dat.ans}>
+                <div className={styles.options}>
+                  <label className={styles.label} for={dat.ans}>
                     <input
                       type="radio"
                       value={dat.id}
@@ -50,28 +48,24 @@ const Quiz = () => {
                       onChange={(e) => {
                         settopping(e.target.value);
                       }}
-                      className="accent-[#da3a00]"
+                      className={styles.input}
                     />
-                    <div className="pl-[8px] text-[#da3a00] font-[300]">
-                      {dat.ans}
-                    </div>
+                    <div className={styles.answ}>{dat.ans}</div>
                   </label>
                 </div>
               );
             })}
           </div>
-          <div className="mt-[2rem]">
+          <div className={styles.buttons}>
             <button
-              className="w-[9rem] h-[2.9rem] border-[1px] border-[#da3a00] text-[#da3a00] transition-all duration-200 hover:scale-[1.1]"
+              disabled={qno === 0 ? true : false}
+              className={qno === 0 ? styles.bbleft : styles.bleft}
               onClick={prev}
             >
               Prev
             </button>
-            <button
-              className="w-[9rem] h-[3rem]  bg-[#da3a00] text-white ml-[0.7rem] transition-all duration-200 hover:scale-[1.1]"
-              onClick={next}
-            >
-              Next
+            <button className={styles.bright} onClick={next}>
+              {qno === 16 ? "Submit" : "Next"}
             </button>
           </div>
         </div>
